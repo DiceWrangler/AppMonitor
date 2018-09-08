7953ab721cb82cc235d2e76e4b0286dba9ba5b22
Scott Thorne <dicewrangler@gmail.com>
2018-09-08 13:30:41 -0600

    initial commit

---
+++ AppMonitor/AppMonitor.Designer.vb
@@ -0,0 +1,115 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class AppMonitor
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.cSendTextStatus = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cRouteTextStatus = New System.Windows.Forms.TextBox()
+        Me.cSendTextTimestamp = New System.Windows.Forms.TextBox()
+        Me.cRouteTextTimestamp = New System.Windows.Forms.TextBox()
+        Me.cTimer = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(96, 79)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SendText"
+        '
+        'cSendTextStatus
+        '
+        Me.cSendTextStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cSendTextStatus.Location = New System.Drawing.Point(199, 79)
+        Me.cSendTextStatus.Name = "cSendTextStatus"
+        Me.cSendTextStatus.ReadOnly = True
+        Me.cSendTextStatus.Size = New System.Drawing.Size(100, 22)
+        Me.cSendTextStatus.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(96, 146)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(73, 17)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "RouteText"
+        '
+        'cRouteTextStatus
+        '
+        Me.cRouteTextStatus.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.cRouteTextStatus.Location = New System.Drawing.Point(199, 141)
+        Me.cRouteTextStatus.Name = "cRouteTextStatus"
+        Me.cRouteTextStatus.ReadOnly = True
+        Me.cRouteTextStatus.Size = New System.Drawing.Size(100, 22)
+        Me.cRouteTextStatus.TabIndex = 3
+        '
+        'cSendTextTimestamp
+        '
+        Me.cSendTextTimestamp.Location = New System.Drawing.Point(337, 79)
+        Me.cSendTextTimestamp.Name = "cSendTextTimestamp"
+        Me.cSendTextTimestamp.ReadOnly = True
+        Me.cSendTextTimestamp.Size = New System.Drawing.Size(180, 22)
+        Me.cSendTextTimestamp.TabIndex = 4
+        '
+        'cRouteTextTimestamp
+        '
+        Me.cRouteTextTimestamp.Location = New System.Drawing.Point(337, 146)
+        Me.cRouteTextTimestamp.Name = "cRouteTextTimestamp"
+        Me.cRouteTextTimestamp.ReadOnly = True
+        Me.cRouteTextTimestamp.Size = New System.Drawing.Size(180, 22)
+        Me.cRouteTextTimestamp.TabIndex = 5
+        '
+        'cTimer
+        '
+        '
+        'AppMonitor
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.cRouteTextTimestamp)
+        Me.Controls.Add(Me.cSendTextTimestamp)
+        Me.Controls.Add(Me.cRouteTextStatus)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cSendTextStatus)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "AppMonitor"
+        Me.Text = "AppMonitor"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents cSendTextStatus As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents cRouteTextStatus As TextBox
+    Friend WithEvents cSendTextTimestamp As TextBox
+    Friend WithEvents cRouteTextTimestamp As TextBox
+    Friend WithEvents cTimer As Timer
+End Class
---
+++ AppMonitor/AppMonitor.vb
@@ -0,0 +1,260 @@
+﻿Imports System.Data.SqlClient
+Imports System.Configuration
+
+Public Class AppMonitor
+
+    Public Const APP_NAME As String = "AppMonitor"
+    Const APP_VERSION As String = "v180908"
+    Const APP_STATUS_STARTING As String = "Starting"
+    Const APP_STATUS_RUNNING As String = "Running"
+    Const APP_STATUS_STOPPING As String = "Stopping"
+
+    Structure AppStatus
+        Public AppStatus As String
+        Public Timestamp As String
+    End Structure
+
+    Structure StatusColor
+        Public Foreground As Color
+        Public Background As Color
+    End Structure
+
+    Dim gDBConn As SqlConnection
+    Dim gCycleInterval As Integer
+    Dim gShutdownRequested As Boolean = False
+
+
+    Function GetAppStatus(pAppName As String) As AppStatus
+
+        Dim lAppStatus As AppStatus
+        Dim lCmd As New SqlCommand
+        Dim lReader As SqlDataReader
+
+        lAppStatus = Nothing
+
+        Try
+
+            lCmd = gDBConn.CreateCommand
+
+            lCmd.CommandText = "GetAppStatus"
+            lCmd.CommandType = CommandType.StoredProcedure
+
+            lCmd.Parameters.Add("@AppName", SqlDbType.VarChar)
+            lCmd.Parameters("@AppName").Value = pAppName
+
+            lReader = lCmd.ExecuteReader(CommandBehavior.SingleRow)
+            lReader.Read()
+
+            If lReader.HasRows Then
+
+                With lAppStatus
+
+                    .AppStatus = lReader.GetString(0)
+                    .Timestamp = lReader.GetDateTime(1).ToString
+
+                End With
+
+            End If
+
+        Catch ex As Exception
+
+            MessageBox.Show("GetAppStatus: " & ex.ToString, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+
+        Finally
+
+            lReader = Nothing
+
+            lCmd.Dispose()
+            lCmd = Nothing
+
+        End Try
+
+        GetAppStatus = lAppStatus
+
+    End Function
+
+
+    Function Startup() As Integer
+
+        Dim lError As Integer = 0
+        Dim lAnyError As Integer = 0
+
+        lError = DBOpen()
+        If lError <> 0 Then lAnyError = -1 'If we cannot open the database, flag the error but keep going in case there are more errors during initialization
+
+        Startup = lAnyError
+
+    End Function
+
+
+    Sub Shutdown()
+
+        DBClose()
+
+    End Sub
+
+
+    Public Function DBOpen() As Integer
+
+        Dim lError As Integer = 0
+        Dim lConnectionString As String
+
+        Try
+            lConnectionString = ConfigurationManager.ConnectionStrings("SendText").ConnectionString
+            gDBConn = New SqlConnection(lConnectionString)
+            gDBConn.Open()
+        Catch ex As Exception
+            MessageBox.Show("DBOpen: " & ex.ToString, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            lError = -1 ' Flag failure to open database
+        End Try
+
+        DBOpen = lError
+
+    End Function
+
+
+    Public Sub DBClose()
+
+        Try
+            gDBConn.Close()
+            gDBConn.Dispose()
+        Catch
+            ' No worries if error encountered while closing
+        Finally
+            gDBConn = Nothing
+        End Try
+
+    End Sub
+
+
+    Public Function GetAppConfig(pAppName As String, pConfigName As String, pDefaultValue As String) As String
+
+        Dim lCmd As New SqlCommand
+        Dim lResults As String
+
+        Try
+
+            lCmd = gDBConn.CreateCommand
+
+            lCmd.CommandText = "GetAppConfig"
+            lCmd.CommandType = CommandType.StoredProcedure
+
+            lCmd.Parameters.Add("@AppName", SqlDbType.VarChar)
+            lCmd.Parameters("@AppName").Value = pAppName
+            lCmd.Parameters.Add("@ConfigName", SqlDbType.VarChar)
+            lCmd.Parameters("@ConfigName").Value = pConfigName
+
+            lResults = lCmd.ExecuteScalar
+            If IsNothing(lResults) Then lResults = pDefaultValue 'If configuration not defined just use default value
+
+        Catch ex As Exception
+            MessageBox.Show("GetAppConfig: " & ex.ToString, "WARNING", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            lResults = pDefaultValue ' If we encountered an error just use default value but log it anyway
+
+        Finally
+
+            lCmd.Dispose()
+            lCmd = Nothing
+
+        End Try
+
+        GetAppConfig = lResults
+
+    End Function
+
+
+    Sub LoadAppConfigs()
+
+        Dim lCycleInterval As String
+
+        lCycleInterval = GetAppConfig(APP_NAME, "Cycle_Interval", "10000")
+        If IsNumeric(lCycleInterval) Then
+            gCycleInterval = Val(lCycleInterval)
+        Else
+            gCycleInterval = 10000
+        End If
+
+    End Sub
+
+    Private Sub AppMonitor_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim lError As Integer = 0
+
+        lError = Startup()
+
+        If lError = 0 Then
+            LoadAppConfigs() ' Subroutine just uses defaults if it fails for whatever reason
+            cTimer.Interval = gCycleInterval
+        Else
+            MessageBox.Show(APP_NAME & " is shutting down", "STOP", MessageBoxButtons.OK, MessageBoxIcon.Stop)
+            Shutdown()
+            Application.Exit()
+        End If
+
+    End Sub
+
+
+    Private Sub AppMonitor_Shown(sender As Object, e As EventArgs) Handles MyBase.Shown
+
+        Main_Loop()
+        cTimer.Start()
+
+    End Sub
+
+
+    Private Sub AppMonitor_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+
+        Shutdown()
+
+    End Sub
+
+    Private Sub Tick(sender As Object, e As EventArgs) Handles cTimer.Tick
+
+        Main_Loop()
+
+    End Sub
+
+
+    Sub Main_Loop()
+
+        Dim lAppStatus As AppStatus
+
+        lAppStatus = GetAppStatus("SendText")
+        cSendTextStatus.Text = lAppStatus.AppStatus
+        cSendTextTimestamp.Text = lAppStatus.Timestamp
+        cSendTextStatus.BackColor = GetStatusColor(lAppStatus.AppStatus).Background
+        cSendTextStatus.ForeColor = GetStatusColor(lAppStatus.AppStatus).Foreground
+
+        lAppStatus = GetAppStatus("RouteText")
+        cRouteTextStatus.Text = lAppStatus.AppStatus
+        cRouteTextTimestamp.Text = lAppStatus.Timestamp
+        cRouteTextStatus.BackColor = GetStatusColor(lAppStatus.AppStatus).Background
+        cRouteTextStatus.ForeColor = GetStatusColor(lAppStatus.AppStatus).Foreground
+
+    End Sub
+
+    Function GetStatusColor(pAppStatus As String) As StatusColor
+
+        Dim lStatusColor As New StatusColor
+
+        With lStatusColor
+            Select Case pAppStatus
+                Case APP_STATUS_RUNNING
+                    .Background = Color.Lime
+                    .Foreground = Color.Black
+                Case APP_STATUS_STARTING
+                    .Background = Color.Yellow
+                    .Foreground = Color.Black
+                Case APP_STATUS_STOPPING
+                    .Background = Color.Red
+                    .Foreground = Color.White
+                Case Else
+                    .Background = Color.Black
+                    .Foreground = Color.White
+            End Select
+        End With
+
+        GetStatusColor = lStatusColor
+
+    End Function
+End Class
